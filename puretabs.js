1d880d993bdfd45f737186d19fcd61197e45412c
Andrew Romanov <scorpion21.97@gmail.com>
2015-05-04 18:18:19 +0700

    Add second version, modify README.md, add index.html

--- puretabs.js
+++ puretabs.js
@@ -2,50 +2,65 @@
 
   var $ = document.querySelectorAll.bind(document);
 
-  var toggle = function(e, hash, links, activeClassName) {
-    var elem = document.getElementById(hash);
-
-    $('[data-puretabs]')[0].style.display = 'none';
-    $('[data-puretabs]')[0].removeAttribute('data-puretabs');
-
-    $('.' + activeClassName)[0].classList.remove(activeClassName);
-    e.currentTarget.classList.add(activeClassName);
-
-    elem.setAttribute('data-puretabs', '');
-    elem.style.display = 'block';
-  };
-
   exports.pureTabs = {
+
+    toggle: function(e) {
+      var activeClassName = this.activeClassName,
+          oldSection = $('[data-puretabs]')[0],
+          newSection = $(e.currentTarget.hash)[0];
+
+      removeClass($(activeClassName)[0], activeClassName.substr(1));
+      addClass(e.currentTarget, activeClassName.substr(1));
+
+      oldSection.style.display = 'none';
+      oldSection.removeAttribute('data-puretabs');
+
+      newSection.style.display = 'block';
+      newSection.setAttribute('data-puretabs', '');
+    },
+
     init: function(className, activeClassName) {
+      var self = this;
 
-      var self = this,
-          hashes = [],
-          links = [].slice.call($('.' + className));
+      self.className = '.' + className || '.puretabs';
+      self.activeClassName = '.' + activeClassName || '.puretabs--active';
 
-      links.forEach(function(item) {
-        var hash = item.hash.substr(1);
-        hashes.push(hash);
+      var links = [].slice.call($(self.className));
 
-        item.addEventListener('click', function(e) {
+      links.forEach(function(link) {
+
+        if (!containsClass(link, self.activeClassName.substr(1))) {
+          $(link.hash)[0].style.display = 'none';
+        } else {
+          $(link.hash)[0].setAttribute('data-puretabs', '');
+        }
+
+        link.addEventListener('click', function(e) {
           e.preventDefault();
-
-          toggle(e, hash, links, activeClassName);
+          self.toggle.call(self, e)
         });
 
-        if (item.classList.contains(activeClassName)) {
-          document.getElementById(hash).setAttribute('data-puretabs', '');
-        }
       });
-
-      hashes.forEach(function(item) {
-        var elem = document.getElementById(item);
-
-        if (!elem.attributes['data-puretabs']) {
-          elem.style.display = 'none';
-        }
-      });
-
     }
+
   };
+
+
+  //  Helpers
+  function addClass(e, c) {
+    var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g");
+    if (re.test(e.className)) return;
+    e.className = (e.className + " " + c).replace(/\s+/g, " ").replace(/(^ | $)/g, "")
+  }
+
+  function removeClass(e, c) {
+    var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g");
+    e.className = e.className.replace(re, "$1").replace(/\s+/g, " ").replace(/(^ | $)/g, "");
+  }
+
+  function containsClass(e, c) {
+    var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g");
+    return re.test(e.className) ? true : false;
+  }
 
 })(window);
